c85b4e4e2703fb1c64ba313306566a1ff0669947
revrans <revrans.dhamar.ian@gmail.com>
2024-10-07 15:59:28 +0700

    coba di lokal baru

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h3>revrans</h3>
+      <h3>revrans GCGCGCGC</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
